c8ffb4fd9e31a1362cf80618eb40e4e4fc3f53d7
yundai <yundai@paypal.com>
2015-10-10 16:02:12 +0800

    first release

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "PlayScala"
+
+version := "1.0"
+
+organization := "com.yundai"
+
+scalaVersion := "2.11.7"
+
---
+++ src/main/scala/com/yundai/playscala/playconcurrency/PlayConcurrency.scala
@@ -0,0 +1,9 @@
+package com.yundai.playscala.playconcurrency
+
+/**
+ * Created by yundai on 10/10/15.
+ */
+object PlayConcurrency extends App {
+
+  println("Hello World!")
+}
